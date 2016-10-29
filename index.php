39061616262c935277f352f9ce64db517bbb2b37
Elang Surya <elank37@gmail.com>
2016-10-29 08:40:45 +0700

    halaman Login index

--- index.php
+++ index.php
@@ -1,10 +1,52 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Aplikasi Perpus PHP(PDO)</title>
+	<title>LOGIN HALAMAN</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<!-- <link rel="stylesheet" type="text/css" href="style.css"> -->
 </head>
 <body>
-	<h1>Data satu</h1>
-	<h2>data dua di hapus</h2>
+
+	<div class="container">
+		<div id="loginbox" style="margin-top: 100px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<div class="panel-title">Sign In</div>
+					<div style="float: right; font-size: 80%; position: relative; top: -10px"><a href="#">Forgot password</a></div>
+				</div>
+
+				<div style="padding-top: 30px" class="panel-body">
+					<div style="display: none" id="login-alert" class="alert alert-danger col-sm-12"></div>
+					<form id="loginform" class="form-horizontal" role="form">
+						<div style="margin-bottom: 25px" class="input-group">
+							<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+							<input id="login-username" type="text" class="form-control" name="username" placeholder="username">
+						</div>
+						<div style="margin-bottom: 25px" class="input-group">
+							<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+							<input id="login-password" type="password" class="form-control" name="password" placeholder="password">
+						</div>
+
+						<div style="margin-top: 10px;" class="form-group">
+							<div class="col-sm-12 controls">
+								<button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">Login</button> 
+							</div>
+						</div>
+
+
+						<div class="form-group">
+							<div class="col-md-12 control">
+								<div style="border-top: 1px solid#888; padding-top: 15px; font-size: 85%">
+									PHPOISON.BLOGSPOT.COM
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
 </body>
 </html>
