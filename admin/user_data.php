234ec3d26d3c2e975f4ad22193dbdd6cfa85746c
er1an9 <sun_eagle@ymail.com>
2016-11-01 10:50:54 +0700

    ubah placeholder

--- admin/user_data.php
+++ admin/user_data.php
@@ -9,7 +9,7 @@
 			<div class="pull-right col-md-4">
 				<form action="?page=user_search" method="post">				
           <div class="input-group">
-				  	<input type="text" name="cari" class="form-control" placeholder="Cari nama, Pengarang..">
+				  	<input type="text" name="cari" class="form-control" placeholder="Cari Nama User..">
 				    <span class="input-group-btn">
 				    <button type="submit" class="btn btn-default" type="button">
 				    	<span class="glyphicon glyphicon-search"></span>
