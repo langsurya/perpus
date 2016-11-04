0bbd7f1f32287690da65cadea1eff2202a3889f3
er1an9 <sun_eagle@ymail.com>
2016-11-04 14:19:55 +0700

    add laporan

---
+++ admin/laporan.php
@@ -0,0 +1,23 @@
+<div class="col-sm-9">
+  <h4>Cetak Laporan</h4>
+  <hr>	
+</div>
+<div class="col-md-9">
+	<div class="panel panel-info">
+		<div class="panel-heading">
+			<div class="panel-title">Laporan</div>
+		</div>
+
+		<div class="panel-body">
+		
+		<div class="list-group">		  
+		  <a href="../print-anggota.php" target="_blank" class="list-group-item">Laporan Anggota</a>
+		  <a href="../print-buku.php" target="_blank" class="list-group-item">Laporan Buku</a>
+		  <a href="../print-transaksi.php" target="_blank" class="list-group-item">Laporan Transaksi</a>
+		</div>
+    
+		</div>
+
+	</div>
+</div>
+
